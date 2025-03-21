877fc8412d5e86138074f6e19bbaf9d829477232
cxk228922 <ben042708@gmail.com>
2025-03-21 23:28:44 +0800

    meow

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cxk228922.github.io',
-  base: '/archive/',
+  base: '/',
   trailingSlash: 'always',
   integrations: [
     tailwind({
