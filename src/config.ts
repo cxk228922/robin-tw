c1801ba1df4c483c1da60ec27f683b59bef9ad6e
cxk228922 <ben042708@gmail.com>
2025-04-16 17:42:16 +0800

    Fix avatar path in profileConfig to remove unnecessary public directory reference

--- src/config.ts
+++ src/config.ts
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 }
 
 export const profileConfig: ProfileConfig = {
-  avatar: '/public/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: '/avatar.jpg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'Robin Chiang',
   bio: '花束を',
   links: [
